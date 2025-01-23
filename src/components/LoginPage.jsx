76e7fa5d092a189b0cb320a66a37300c1af0638a
swarnajha <swarnajha2018@gmail.com>
2025-01-23 15:48:14 +0530

    new

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -9,7 +9,9 @@ export default function LoginPage() {
           "radial-gradient(circle, #5CBBFF 0%,#0094FF 31%, #FFFFFF 100%)",
       }}
     >
-      <h1 className="text-3xl font-bold mb-8 text-gray-900">GENAI UNIPLANE</h1>
+      <h1 className="text-3xl font-bold mb-8 text-gray-900">
+        GENAI LLMOps Services
+      </h1>
       <Link
         to="/login"
         className="bg-white px-8 py-3 rounded-full text-gray-900 font-medium shadow-lg hover:shadow-xl transition-shadow duration-300 hover:bg-gray-50"
